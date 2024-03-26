d80d5534ff7a0fdd42b6e2a30dd1ad1f3a0de8be
GRIFFITHH <glqglq032@gmail.com>
2024-03-26 21:03:57 +0900

    파이썬 1일차

---
+++ python_study1.py
@@ -0,0 +1,38 @@
+print('Python start')
+print("Python Start")
+print('''Python Start''')
+print('p','y','t','h','o','n', sep=' ')
+print('010','2243','3421', sep='-')
+
+
+print('welcome to', end=' ') , print('IT News',end=' ')
+
+import sys
+
+print('Learn Python', file=sys.stdout)
+
+
+#format (d : 3 , s : 'python' , f : 3.1444)
+
+print('%s %s' % ('one','2'))
+
+print('{} {}' .format('one','2'))
+
+print('{1} {0}' .format('one','2'))
+
+print('%10s' % ('nice'))
+
+print('{:>10}' .format('nice'))
+
+print('%-10s' % ('nice'))
+
+print('{:10}' .format('nice'))
+
+print('%+10s' % ('nice'))
+
+print('{:_>10}' .format('nice'))
+print('{:^10}' .format('nice'))
+print('%.5s' % ('nice body'))
+print('%5s' % ('nice body'))
+
+print('{:^10.5}' .format('pythonstudy'))
